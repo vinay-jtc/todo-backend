ea570e4120be7cecd767a0225061282868fa7dfe
vinay-jtc <88647872+vinay-jtc@users.noreply.github.com>
2021-08-13 16:21:46 +0530

    End point to fetch todo item (#2)
    * todo item fatch
    
    * test added and validation
    
    * changed code
    
    * error deleted from en.json
    
    * test file change
    
    * test cases run | mongoID validation
    
    * end line added
    
    * resolve

--- src/controllers/todo-item-controller.ts
+++ src/controllers/todo-item-controller.ts
@@ -9,7 +9,8 @@ import {
   ValidationFailure,
 } from '@typings';
 import {
-  createTodoItemValidator
+  createTodoItemValidator,
+  fetchTodoItemValidator
 } from '@validators';
 
 export class TodoItemController extends BaseController {
@@ -27,8 +28,35 @@ export class TodoItemController extends BaseController {
       createTodoItemValidator(),
       this.createTodoItem,
     );
+
+    this.router.get(
+      `${this.basePath}/:id`,
+      fetchTodoItemValidator(this.appContext),
+      this.fetchTodoItem,
+    );
   }
 
+  private fetchTodoItem = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
+    const failures: ValidationFailure[] = Validation.extractValidationErrors(req);
+    if (failures.length > 0) {
+      const valError = new Errors.ValidationError(
+        res.__("DEFAULT_ERRORS.VALIDATION_FAILED"),
+        failures
+      );
+      return next(valError);
+    }
+
+    const { id } = req.params;
+    const todoItem = await this.appContext.TodoItemRepository.findById(id);
+    if (todoItem?._id) {
+      res.status(200).json(todoItem.serialize());
+    }else{
+      const valError = new Errors.NotFoundError(res.__('DEFAULT_ERRORS.RESOURCE_NOT_FOUND'));
+      return next(valError);
+    }
+  };
+
+  
   private createTodoItem = async (
     req: ExtendedRequest,
     res: Response,
--- test/spec/todo/todo.spec.ts
+++ test/spec/todo/todo.spec.ts
@@ -9,6 +9,7 @@ import { Application } from "express";
 import { respositoryContext, testAppContext } from "../../mocks/app-context";
 
 import { App } from "@server";
+import { TodoItem } from "@models";
 
 chai.use(chaiHttp);
 const expect = chai.expect;
@@ -43,18 +44,33 @@ describe("POST /todos", () => {
       .to.have.nested.property("failures[0].message")
       .to.equal("Please specify the valid title");
   });
-  
-  it("should return a validation error if title is not a string", async () => {
-    const res = await chai
-      .request(expressApp)
-      .post("/todos")
-      .send({
-        title: { key: "value" },
-      });
+});
 
+describe("GET /todos/:id", () => {
+  it("should fetch a todo item if it exists and if id is valid mongo id", async () => {
+    const todoItem = await testAppContext.TodoItemRepository.save(
+      new TodoItem({ title: "Fetching an item" })
+    );
+
+    const res = await chai.request(expressApp).get(`/todos/${todoItem._id}`);
+    expect(res).to.have.status(200);
+    expect(res.body).to.have.property("id");
+    expect(res.body).to.have.property("title");
+  });
+
+  it("Should return a validation error if id is invalid mongo id", async () => {
+    const res = await chai.request(expressApp).get("/todos/befji47crhjehr");
     expect(res).to.have.status(400);
     expect(res.body)
       .to.have.nested.property("failures[0].message")
-      .to.equal("Please specify the valid title");
+      .to.equal("Mongo ID is invalid");
+  });
+
+  it("should return a 404 if todo item does not exists", async () => {
+    const res = await chai
+      .request(expressApp)
+      .get("/todos/605bb3efc93d78b7f4388c2c");
+
+    expect(res).to.have.status(404);
   });
 });
